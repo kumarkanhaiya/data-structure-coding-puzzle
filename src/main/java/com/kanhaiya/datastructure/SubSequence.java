2c014d9ec67e5d2270fa1f59b82d57369e8a3e93
zw27b06 <kumarkanhaiya@johndeere.com>
2022-03-02 15:01:35 +0530

    updated SubSequence.java

--- src/main/java/com/kanhaiya/datastructure/SubSequence.java
+++ src/main/java/com/kanhaiya/datastructure/SubSequence.java
@@ -6,12 +6,12 @@ import java.util.Objects;
 
 public class SubSequence {
     public static void main(String[] args) {
-        List arrayI = Arrays.asList(5, 1, 22, 25, 6, -1, 8, 10);
+        List arrayI = Arrays.asList(5, 1, 22, 25, 6, -1, 8, 10, 11, 11, 11, 11);
         List sequenceI = Arrays.asList(1, 6, -1, 10, 11, 11, 11, 11);
         System.out.println(isValidSubsequence(arrayI, sequenceI));
     }
 
-    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+    /*public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
         // Write your code here.
         if(sequence.size() > array.size())
             return false;
@@ -36,5 +36,18 @@ public class SubSequence {
             return true;
         }
         return false;
+    }*/
+
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        // Write your code here.
+        int arrIndex = 0;
+        int seqIndex = 0;
+        while (arrIndex < array.size() && seqIndex < sequence.size()){
+            if(sequence.get(seqIndex) == array.get(arrIndex)){
+               seqIndex++;
+            }
+            arrIndex++;
+        }
+        return seqIndex == sequence.size();
     }
 }
