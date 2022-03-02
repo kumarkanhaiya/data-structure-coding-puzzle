f24273244ca550c038b2aa699e48778f3df687e3
zw27b06 <kumarkanhaiya@johndeere.com>
2022-03-02 14:47:14 +0530

    SubSequence.java

---
+++ src/main/java/com/kanhaiya/datastructure/SubSequence.java
@@ -0,0 +1,40 @@
+package com.kanhaiya.datastructure;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class SubSequence {
+    public static void main(String[] args) {
+        List arrayI = Arrays.asList(5, 1, 22, 25, 6, -1, 8, 10);
+        List sequenceI = Arrays.asList(1, 6, -1, 10, 11, 11, 11, 11);
+        System.out.println(isValidSubsequence(arrayI, sequenceI));
+    }
+
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        // Write your code here.
+        if(sequence.size() > array.size())
+            return false;
+
+        int i = 0;
+        int j = 0;
+
+        for(; i < sequence.size() && j < array.size(); i++){
+            boolean isFound = false;
+            for(; j < array.size(); j++) {
+                if(Objects.equals(sequence.get(i), array.get(j))){
+                    isFound = true;
+                    j++;
+                    break;
+                }
+            }
+            if(!isFound) {
+                return false;
+            }
+        }
+        if(i == sequence.size()) {
+            return true;
+        }
+        return false;
+    }
+}
